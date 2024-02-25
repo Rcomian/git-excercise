8351ded20e30c2a5ecda9ecbccf98f35e1c30d92
Jim T <github@tupper.org.uk>
2024-02-25 19:29:29 +0000

    Adding time feature to the date printer

--- datePrinter.js
+++ datePrinter.js
@@ -1,5 +1,7 @@
 function printDate() {
-    console.log(`Today's date is: ${new Date().toLocaleDateString()}`);
+    const now = new Date();
+    console.log(`Today's date is: ${now.toLocaleDateString()}`);
+    console.log(`Current time is: ${now.toLocaleTimeString()}`);
 }
 
 module.exports = printDate;

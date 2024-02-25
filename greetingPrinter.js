8a6e1dc0f03c5425ce42e082a153abc593260f91
Jim T <github@tupper.org.uk>
2024-02-25 19:34:19 +0000

    Adding random greetings

--- greetingPrinter.js
+++ greetingPrinter.js
@@ -1,5 +1,15 @@
 function printGreeting() {
-    console.log("Hello, welcome to our demo project!");
+    const greetings = [
+        "Hello, welcome to our demo project!",
+        "Hi there! Glad you're checking this out.",
+        "Greetings, explorer! Dive into our project.",
+        "Welcome aboard! Enjoy our little demo.",
+        "Hey! Hope you find this interesting."
+    ];
+
+    // Select a random greeting
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    console.log(greeting);
 }
 
 module.exports = printGreeting;

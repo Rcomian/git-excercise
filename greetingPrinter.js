8c376429e10f24ef8abe7fdf9edc41568a4515c4
Jim T <github@tupper.org.uk>
2024-02-25 19:36:36 +0000

    Adding extra greetings

--- greetingPrinter.js
+++ greetingPrinter.js
@@ -4,7 +4,12 @@ function printGreeting() {
         "Hi there! Glad you're checking this out.",
         "Greetings, explorer! Dive into our project.",
         "Welcome aboard! Enjoy our little demo.",
-        "Hey! Hope you find this interesting."
+        "Hey! Hope you find this interesting.",
+        "Good day! Let's explore something new today.",
+        "Hello! Excited to share our work with you.",
+        "Welcome! Curiosity is the best guide.",
+        "Salutations! You're in for a treat.",
+        "Hi! Let's make today a productive day."
     ];
 
     // Select a random greeting
